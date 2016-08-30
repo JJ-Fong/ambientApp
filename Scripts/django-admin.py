4b4ab2dc0ae5af5c3b4e930c7c65ffbb69f01ec8
JJ-Fong <jjosuefong@gmail.com>
2016-08-30 15:55:35 -0600

    basic-template-02

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\javier~1\docume~1\univer~1\2016\ciclo2~1\ambien~1\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
